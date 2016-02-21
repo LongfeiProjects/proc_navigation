af92abd42780ea17245d4a132520e4712d4cebca
Adrien Kerroux <adrienkerroux@gmail.com>
2016-02-21 12:21:45 -0500

    Change P0 and Qc sizes

--- src/proc_navigation/kalman/extended_kalman_filter.cc
+++ src/proc_navigation/kalman/extended_kalman_filter.cc
@@ -62,7 +62,7 @@ ExtendedKalmanFilter::ExtendedKalmanFilter(
 
 //------------------------------------------------------------------------------
 //
-ExtendedKalmanFilter::~ExtendedKalmanFilter() ATLAS_NOEXCEPT {}
+ExtendedKalmanFilter::~ExtendedKalmanFilter() ATLAS_NOEXCEPT { }
 
 //==============================================================================
 // M E T H O D   S E C T I O N
@@ -70,15 +70,15 @@ ExtendedKalmanFilter::~ExtendedKalmanFilter() ATLAS_NOEXCEPT {}
 //------------------------------------------------------------------------------
 //
 void ExtendedKalmanFilter::OnSubjectNotify(atlas::Subject<> &subject)
-    ATLAS_NOEXCEPT {
+ATLAS_NOEXCEPT {
   if (dynamic_cast<StateController<BaroMessage> *>(&subject) != nullptr &&
       active_baro) {
     UpdateBaroData();
   } else if (dynamic_cast<StateController<DvlMessage> *>(&subject) != nullptr &&
-             active_dvl) {
+      active_dvl) {
     UpdateDvlData();
   } else if (dynamic_cast<StateController<MagMessage> *>(&subject) != nullptr &&
-             active_mag) {
+      active_mag) {
     UpdateMagData();
   } else if (dynamic_cast<StateController<ImuMessage> *>(&subject) != nullptr) {
     UpdateImuData();
@@ -105,36 +105,36 @@ void ExtendedKalmanFilter::Initialize() {
   x0_.gyro_bias = Eigen::Vector3d(0, 0, 0);
   x0_.baro_bias = 0;
 
-  qc_(0) = sigma_meas_acc;
-  qc_(1) = sigma_meas_acc;
-  qc_(2) = sigma_meas_acc;
-  qc_(3) = sigma_meas_gyr;
-  qc_(4) = sigma_meas_gyr;
-  qc_(5) = sigma_meas_gyr;
-  qc_(6) = sigma_walk_bias_acc;
-  qc_(7) = sigma_walk_bias_acc;
-  qc_(8) = sigma_walk_bias_acc;
-  qc_(9) = sigma_walk_bias_gyr;
-  qc_(10) = sigma_walk_bias_gyr;
-  qc_(11) = sigma_walk_bias_gyr;
-  qc_(12) = sigma_walk_bias_baro;
+  qc_(0, 0) = sigma_meas_acc;
+  qc_(1, 1) = sigma_meas_acc;
+  qc_(2, 2) = sigma_meas_acc;
+  qc_(3, 3) = sigma_meas_gyr;
+  qc_(4, 4) = sigma_meas_gyr;
+  qc_(5, 5) = sigma_meas_gyr;
+  qc_(6, 6) = sigma_walk_bias_acc;
+  qc_(7, 7) = sigma_walk_bias_acc;
+  qc_(8, 8) = sigma_walk_bias_acc;
+  qc_(9, 9) = sigma_walk_bias_gyr;
+  qc_(10, 10) = sigma_walk_bias_gyr;
+  qc_(11, 11) = sigma_walk_bias_gyr;
+  qc_(12, 12) = sigma_walk_bias_baro;
 
-  p0_(0) = sigma0_pos_x;
-  p0_(1) = sigma0_pos_y;
-  p0_(2) = sigma0_pos_z;
-  p0_(3) = sigma0_vel_x;
-  p0_(4) = sigma0_vel_y;
-  p0_(5) = sigma0_vel_z;
-  p0_(6) = sigma0_rho_x;
-  p0_(7) = sigma0_rho_y;
-  p0_(8) = sigma0_rho_z;
-  p0_(9) = sigma0_bias_acc;
-  p0_(10) = sigma0_bias_acc;
-  p0_(11) = sigma0_bias_acc;
-  p0_(12) = sigma0_bias_gyr;
-  p0_(12) = sigma0_bias_gyr;
-  p0_(12) = sigma0_bias_gyr;
-  p0_(12) = sigma0_bias_baro;
+  p0_(0, 0) = sigma0_pos_x;
+  p0_(1, 1) = sigma0_pos_y;
+  p0_(2, 2) = sigma0_pos_z;
+  p0_(3, 3) = sigma0_vel_x;
+  p0_(4, 4) = sigma0_vel_y;
+  p0_(5, 5) = sigma0_vel_z;
+  p0_(6, 6) = sigma0_rho_x;
+  p0_(7, 7) = sigma0_rho_y;
+  p0_(8, 8) = sigma0_rho_z;
+  p0_(9, 9) = sigma0_bias_acc;
+  p0_(10, 10) = sigma0_bias_acc;
+  p0_(11, 11) = sigma0_bias_acc;
+  p0_(12, 12) = sigma0_bias_gyr;
+  p0_(13, 13) = sigma0_bias_gyr;
+  p0_(14, 14) = sigma0_bias_gyr;
+  p0_(15, 15) = sigma0_bias_baro;
 }
 
 //------------------------------------------------------------------------------
@@ -183,14 +183,14 @@ Eigen::Quaterniond ExtendedKalmanFilter::CalculateInitialRotationMatrix(
   Eigen::Matrix3d r0_bn;
   r0_bn(0, 0) = std::cos(pitch) * std::cos(yaw);
   r0_bn(0, 1) = std::sin(roll) * std::sin(pitch) * std::cos(yaw) -
-                std::cos(roll) * std::sin(yaw);
+      std::cos(roll) * std::sin(yaw);
   r0_bn(0, 2) = std::cos(roll) * std::sin(pitch) * std::cos(yaw) +
-                std::sin(roll) * std::sin(yaw);
+      std::sin(roll) * std::sin(yaw);
   r0_bn(1, 0) = std::cos(pitch) * std::sin(yaw);
   r0_bn(1, 1) = std::sin(roll) * std::sin(pitch) * std::sin(yaw) +
-                std::cos(roll) * std::cos(yaw);
+      std::cos(roll) * std::cos(yaw);
   r0_bn(1, 2) = std::cos(roll) * std::sin(pitch) * std::sin(yaw) -
-                std::sin(roll) * std::cos(yaw);
+      std::sin(roll) * std::cos(yaw);
   r0_bn(2, 0) = -std::sin(pitch);
   r0_bn(2, 1) = std::sin(roll) * std::cos(pitch);
   r0_bn(2, 2) = std::cos(roll) * std::cos(pitch);
@@ -240,7 +240,7 @@ void ExtendedKalmanFilter::UpdateMagData() ATLAS_NOEXCEPT {
 //
 bool ExtendedKalmanFilter::IsNewDataReady() const ATLAS_NOEXCEPT {
   return baro_->IsNewDataReady() || imu_->IsNewDataReady() ||
-         dvl_->IsNewDataReady() || mag_->IsNewDataReady();
+      dvl_->IsNewDataReady() || mag_->IsNewDataReady();
 }
 
 }  // namespace proc_navigation
