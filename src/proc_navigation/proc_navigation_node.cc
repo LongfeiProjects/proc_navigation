7e2dbc6b186c1d882575e3d7705c5cfb0f423b59
Jay <jeremie.st.jules.prevost@gmail.com>
2016-03-31 20:31:08 -0400

    Changing the frame id from odom to NED.
    The frame id is the reference frame, so when we move, from where are we moving. When the yaw change, it is by reference to what. The submarine works following the NED convention. When visualizing with RViz, the script ENU_to_NED should be running. ROS (by extent, RViz) is in ENU, so we need to convert the base axis reference to NED with the script to visualize correctly the data (X being real X, Z going down, etc.)

--- src/proc_navigation/proc_navigation_node.cc
+++ src/proc_navigation/proc_navigation_node.cc
@@ -83,7 +83,7 @@ void ProcNavigationNode::OnSubjectNotify(atlas::Subject<> &subject)
 
   nav_msgs::Odometry odom;
   odom.header.stamp = current_time;
-  odom.header.frame_id = "odom";
+  odom.header.frame_id = "NED";
 
   odom.twist.twist.linear.x = state.vel_n(0);
   odom.twist.twist.linear.y = state.vel_n(1);
