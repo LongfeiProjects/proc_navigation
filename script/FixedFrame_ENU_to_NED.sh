9d7e4b1ed3d24b0f0773e9716a82e4defd37aee0
Thibaut Mattio <thibaut.mattio@gmail.com>
2016-04-02 14:33:27 -0400

    Deleting the FixedFrame script to be put in the launch file

--- script/FixedFrame_ENU_to_NED.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Usage: static_transform_publisher x y z yaw pitch roll frame_id child_frame_id  period(milliseconds)
-#
-# Yaw is rotation about Z. Pitch is rotation about Y. Roll is rotation about Z.
-# Use the right hand rule, thumb in the direction of the axis and the finger curled aroud the axis.
-# The direction of the finger is the the direction "plus" of the rotation.
-
-rosrun tf static_transform_publisher 0.0 0.0 0.0 0.0 0.0 3.1415926535898 ENU NED 100
